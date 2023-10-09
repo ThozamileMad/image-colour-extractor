c5fbb7e5d1b7dbbb22b10daeb7daa218f91cd7b5
ThozamileMad <118363197+ThozamileMad@users.noreply.github.com>
2023-10-09 09:53:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from data_processor import generate_array, get_random_code
+from data_processor import generate_array, get_common_codes
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "SBAALAV327IUMWSLMD;LMDJIOHOHND"
@@ -13,12 +13,8 @@ def home():
 @app.route("/<file>/<int:range_num>", methods=["GET", "POST"])
 def display(file, range_num):
     array = generate_array(f"static/images/{file}")
-    if range_num < 30:
-        codes = get_random_code(array, range_num)
-        return render_template("display.html", codes=codes, file=file)
-    else:
-        codes = get_random_code(array, 30)
-        return render_template("display.html", codes=codes, file=file)
+    codes = get_common_codes(array, range_num)
+    return render_template("display.html", codes=codes, file=file)
 
 
 @app.route("/upload/<error>", methods=["GET", "POST"])
@@ -31,8 +27,6 @@ def upload(error):
             return redirect(url_for("upload", error="true"))
 
         if filename == "":
-            return redirect(url_for("upload", error="true"))
-        elif range_num > 30:
             return redirect(url_for("upload", error="true"))
         else:
             if "file" in request.files:

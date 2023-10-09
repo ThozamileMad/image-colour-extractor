b847034c3a5c04e2b5b3acfed7133464d2fc546d
ThozamileMad <118363197+ThozamileMad@users.noreply.github.com>
2023-10-09 09:54:29 +0200

    Update data_processor.py

--- data_processor.py
+++ data_processor.py
@@ -1,24 +1,25 @@
 import numpy as np
 from PIL import Image
-import random
+from collections import Counter
 
 
-def generate_array(path="art.jpg"):
+def generate_array(path):
     pillow_img = Image.open(path)
     color_array = np.array(pillow_img)
     return color_array
 
 
-def get_random_code(colour_array, range_num):
+def get_common_codes(colour_array, range_num):
     codes = []
     for lst1 in colour_array:
         for lst2 in lst1:
             codes.extend(lst2)
-    non_white_codes = [code for code in codes if code > 100]
+    none_white_codes = [code for code in codes if code > 100]
+    most_common = Counter(none_white_codes).most_common()[:range_num]
 
-    random_color_codes = [random.choice(non_white_codes) for num in range(range_num)]
+    return most_common
 
-    return random_color_codes
+
 
 
 
